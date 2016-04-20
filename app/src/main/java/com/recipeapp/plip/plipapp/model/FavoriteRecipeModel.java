1cadf8de60c0c8eb78ae95c24c6e24d2d90fccfd
Sugar Palaces <SugarPalaces@wrls-106-136.sonoma.edu>
2016-04-19 18:06:18 -0700

    Added Favorite Recipe Model

---
+++ app/src/main/java/com/recipeapp/plip/plipapp/model/FavoriteRecipeModel.java
@@ -0,0 +1,18 @@
+package com.recipeapp.plip.plipapp.model;
+
+/**
+ * Created by SugarPalaces on 4/19/16.
+ */
+public class FavoriteRecipeModel {
+    //private Integer id;
+    private String recipe_name;
+    private String notes;
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
